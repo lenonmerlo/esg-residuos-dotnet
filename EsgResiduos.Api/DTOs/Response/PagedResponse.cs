42a72f5fb7133065ca3d3b053ba6cbca05527ae5
lenonmerlo <lenontm@gmail.com>
2026-06-10 08:55:37 -0300

    fix: improve exception messages and update PagedResponse

--- EsgResiduos.Api/DTOs/Response/PagedResponse.cs
+++ EsgResiduos.Api/DTOs/Response/PagedResponse.cs
@@ -2,7 +2,7 @@ namespace EsgResiduos.Api.DTOs.Response;
 
 public class PagedResponse<T>
 {
-    public IEnumerable<T> Data { get; set; } = Enumerable.Empty<T>();
+    public IEnumerable<T> Data { get; set; } = [];
     public int Page { get; set; }
     public int PageSize { get; set; }
     public int TotalCount { get; set; }
